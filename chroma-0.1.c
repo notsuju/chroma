97d5919e3d2add89aa753782945a3780c1103e21
notsuju <notsuju@proton.me>
2024-11-17 17:45:31 +0530

    "fixed many issues"

--- chroma-0.1.c
+++ chroma-0.1.c
@@ -8,10 +8,6 @@
 #define MAX_LINE_LENGTH 1024
 
 // FUNCTIONS
-void move_cursor_up();
-void move_cursor_down();
-void move_cursor_right();
-void move_cursor_left();
 void set_cursor_position(int r, int c);
 void enable_raw_mode();
 void disable_raw_mode();
@@ -20,29 +16,67 @@ struct termios orig_termios;
 
 int main(int argc, char *argv[])
 {
-    system("clear");
     if (argc != 2)
     {
-        printf("Usage: ./chroma filename\n");
+        printf("Use './chroma-0.1 help' for more information\n");
         return 1;
     }
+    // Help prompt
+    if (strcmp(argv[1], "help") == 0)
+    {
+        printf(" Usage: './chroma-0.1 file_name'\n\n");
+        printf(" Command Mode: \n\n");
+        printf(" k --move up\n j --move down\n l --move right\n h --move left\n s --save file\n q --quit the program\n i --edit mode\n\n");
+        printf(" Edit Mode: \n\n");
+        printf(" esc --quit edit mode and enter command mode\n");
+        return -1;
+    }
+
+    system("clear");
+    // Enabling raw mode to read keystrokes without waiting for enter key
     enable_raw_mode();
+
     FILE *input = fopen(argv[1], "r");
     if (input == NULL)
     {
-        printf("Error opening the file\n");
+        FILE *new = fopen(argv[1], "w");
+        fclose(new);
+    }
+
+    char **buffer = (char **)malloc(MAX_LINES * sizeof(char *));
+    for (int i = 0; i < MAX_LINES; i++)
+    {
+        buffer[i] = (char *)malloc(MAX_LINE_LENGTH * sizeof(char));
+    }
+    if (buffer == NULL)
+    {
+        printf("Error allocating memory\n");
+        disable_raw_mode();
         return 2;
     }
-    char buffer[MAX_LINES][MAX_LINE_LENGTH];
     int line_no = 0;
-    // char *buffer = (char*)malloc(1024 * sizeof(char));
-    while (fgets(buffer[line_no], MAX_LINE_LENGTH, input) != NULL)
+
+    if (input != NULL)
     {
-        line_no++;
-    }
-    for (int i = 0; i < line_no; i++)
-    {
-        printf("%s", buffer[i]);
+        while (fgets(buffer[line_no], MAX_LINE_LENGTH, input) != NULL)
+        {
+            line_no++;
+        }
+        for (int i = 0; i < line_no; i++)
+        {
+            printf("%s", buffer[i]);
+        }
+
+        // adding a newline and null terminator to the last line of the file
+        int last_line_length = strlen(buffer[line_no - 1]);
+        if (buffer[line_no - 1][last_line_length - 1] != '\n')
+        {
+            if (last_line_length + 1 <= MAX_LINE_LENGTH)
+            {
+                buffer[line_no - 1][last_line_length] = '\n';
+                buffer[line_no - 1][last_line_length + 1] = '\0';
+            }
+        }
     }
 
     // Navigation
@@ -50,42 +84,57 @@ int main(int argc, char *argv[])
     int r = 1;
     int c = 1;
     int editing = 0;
-    int counter;
+    int insert_mode;
     while (1)
     {
+        // DISPLAY THE FILE AFTER EVERY CHANGE
+        system("clear");
+        for (int i = 0; i < line_no; i++)
+        {
+            printf("%s", buffer[i]);
+        }
+
+        // Setting the cursor to the correct position
         set_cursor_position(r, c);
 
         keystroke = getchar();
+
         // COMMAND MODE
         if (!editing)
         {
-            counter = 0;
+            insert_mode = 0;
             if (keystroke == 'k' && r > 1)
             {
-                move_cursor_up();
-                r--;
+                if (c > strlen(buffer[r - 2]))
+                {
+                    c = strlen(buffer[r - 2]);
+                    r--;
+                }
+                else
+                {
+                    r--;
+                }
             }
             else if (keystroke == 'j' && r < line_no)
             {
-                move_cursor_down();
-                r++;
+                if (c > strlen(buffer[r]))
+                {
+                    c = strlen(buffer[r]);
+                    r++;
+                }
+                else
+                {
+                    r++;
+                }
             }
             else if (keystroke == 'l' && c < strlen(buffer[r - 1]))
             {
-                move_cursor_right();
                 c++;
             }
             else if (keystroke == 'h' && c > 1)
             {
-                move_cursor_left();
                 c--;
             }
-            // else if (keystroke == 'x')
-            // {
-            //     scanf(" %d", &r);
-            //     scanf(" %d", &c);
-            // }
-
             else if (keystroke == 'q')
             {
                 break;
@@ -96,11 +145,11 @@ int main(int argc, char *argv[])
             }
             else if (keystroke == 's')
             {
-                // fclose(input);
                 FILE *output = fopen(argv[1], "w");
                 if (output == NULL)
                 {
                     printf("Error saving file");
+                    disable_raw_mode();
                     return 3;
                 }
                 for (int i = 0; i < line_no; i++)
@@ -136,12 +185,24 @@ int main(int argc, char *argv[])
                 }
                 else
                 {
-                    for (int i = c; i < MAX_LINE_LENGTH; i++)
-                    {
-                        buffer[r - 1][i - 1] = buffer[r - 1][i];
-                    }
+                    int line_length = strlen(buffer[r - 2]);
+                    // Setting the cursor to the end of the previous line
                     r--;
                     c = strlen(buffer[r - 1]);
+
+                    // Removing the endline character from the previous line
+                    buffer[r - 1][line_length - 1] = '\0';
+
+                    // Concatenating the previous line with the current line
+                    strcat(buffer[r - 1], buffer[r]);
+
+                    // Shifting all the lines up
+                    for (int i = r; i < line_no; i++)
+                    {
+                        memset(buffer[i], '\0', MAX_LINE_LENGTH);
+                        strcpy(buffer[i], buffer[i + 1]);
+                    }
+                    line_no--;
                 }
             }
             // NEW LINE
@@ -151,7 +212,8 @@ int main(int argc, char *argv[])
                 // Shift all lines down
                 for (int i = line_no; i > r; i--)
                 {
-                    memset(buffer[i], '\0', MAX_LINE_LENGTH); // Clears the line and set it to null to avoid overlapping
+                    // Clearing the line to avoid overlapping
+                    memset(buffer[i], '\0', MAX_LINE_LENGTH);
                     strcpy(buffer[i], buffer[i - 1]);
                 }
 
@@ -168,7 +230,7 @@ int main(int argc, char *argv[])
                     buffer[r - 1][line_length - 1] = '\n';
                     buffer[r - 1][line_length] = '\0';
                     buffer[r][0] = '\n';
-                    buffer[r][1] = '\0';
+                    buffer[r][0] = '\0';
                 }
 
                 r++;
@@ -178,52 +240,40 @@ int main(int argc, char *argv[])
             // INSERT CHARACTER
             else if (c < MAX_LINE_LENGTH)
             {
-                if (counter)
+                if (insert_mode)
                 {
                     int line_length = strlen(buffer[r - 1]);
-                    if (line_length > 0)
+                    // For handling empty first line
+                    if (c == 1 && r == 1 && line_length == 0)
                     {
-                        for (int i = line_length; i >= c - 1; i--)
-                        {
-                            buffer[r - 1][i] = buffer[r - 1][i - 1];
-                        }
+                        buffer[0][0] = '\n';
+                        buffer[0][1] = '\0';
+                        line_no++;
+                        line_length++;
+                    }
+                    for (int i = line_length; i >= c - 1; i--)
+                    {
+                        buffer[r - 1][i] = buffer[r - 1][i - 1];
                     }
                     buffer[r - 1][c - 1] = keystroke;
                     c++;
                 }
-                counter = 1;
+                insert_mode = 1;
             }
-        }
-        // DISPLAY THE FILE AFTER EVERY CHANGE
-        system("clear");
-        for (int i = 0; i < line_no; i++)
-        {
-            printf("%s", buffer[i]);
         }
     }
     disable_raw_mode();
-    fclose(input);
+    if (input != NULL)
+    {
+        fclose(input);
+    }
+    for (int i = 0; i < MAX_LINES; i++)
+    {
+        free(buffer[i]);
+    }
+    free(buffer);
+    buffer = NULL;
     return 0;
-}
-
-void move_cursor_up()
-{
-    printf("\033[A");
-}
-
-void move_cursor_down()
-{
-    printf("\033[B");
-}
-
-void move_cursor_right()
-{
-    printf("\033[C");
-}
-
-void move_cursor_left()
-{
-    printf("\033[D");
 }
 
 void set_cursor_position(int r, int c)
